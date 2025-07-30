b6270de9682e2a41c994d40d9cba8c9bc3204dd7
Piyush0823 <162396912+Piyush0823@users.noreply.github.com>
2025-07-30 07:48:51 +0530

    Delete matcher_similarity.py

--- matcher_similarity.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def get_similarity_score(resume, jd):
-    vectorizer = TfidfVectorizer(stop_words='english')
-    vectors = vectorizer.fit_transform([resume, jd])
-    score = cosine_similarity(vectors[0], vectors[1])[0][0]
-    return score, vectorizer
